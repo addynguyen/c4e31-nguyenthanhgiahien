18d6852fdb25700d975eda9c499fe1cfb06e79dd
addynguyen <hiennguyen.teamx@gmail.com>
2019-04-18 22:18:37 +0700

    .....

---
+++ session1/index.js
@@ -0,0 +1,21 @@
+// const X = prompt("Enter X");
+// const Y = prompt("Enter Y");
+
+// console.log("SUM =",Number(X) + Number(Y));
+
+// for (let i=0; i < 3; i++) {
+//     fd(100)
+//     rt(90)
+//     fd(100)
+//     rt(90)fd(100)
+//     rt(90)fd(100)
+//     rt(90)
+
+//     rt(60)
+// }
+
+
+for (let i=0; i<5; i++) {
+    console.log(i)
+    console.log("Hello")
+}
